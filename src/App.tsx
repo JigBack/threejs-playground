c6a8caee6ac2018e3d7f7d3943784b450e47365e
Jack Robinson <jackrobinson920@gmail.com>
2022-04-02 21:27:19 +0100

    grommet added

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,33 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ExamplePage from './pages/ExamplePage'
 import HomePage from './pages/HomePage'
 import './App.css';
+import { Button, Grommet, Header, Menu } from 'grommet';
+import { Home } from 'grommet-icons';
+
+const theme = {
+  global: {
+    font: {
+      family: 'Roboto',
+      size: '18px',
+      height: '20px',
+    },
+  },
+};
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/example" element={<ExamplePage />} />
-      </Routes>
-    </BrowserRouter>
+    <Grommet theme={theme}>
+      <Header background="brand">
+        <Button icon={<Home />} hoverIndicator />
+        <Menu label="account" items={[{ label: 'logout' }]} />
+      </Header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/example" element={<ExamplePage />} />
+        </Routes>
+      </BrowserRouter>
+    </Grommet>
   );
 }
 
