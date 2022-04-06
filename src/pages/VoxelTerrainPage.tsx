e635762446f1d1b1368f7d40bccc3217b96b197a
Jack Robinson <jackrobinson920@gmail.com>
2022-04-06 23:01:18 +0100

    controls added to voxel page

--- src/pages/VoxelTerrainPage.tsx
+++ src/pages/VoxelTerrainPage.tsx
@@ -1,43 +1,49 @@
-import * as THREE from 'three'
-import React, { useRef, useState } from 'react'
-import { Canvas, MeshProps, useFrame } from '@react-three/fiber'
+import React, { ChangeEventHandler, useState } from 'react'
+import { Canvas, MeshProps } from '@react-three/fiber'
 import SimplexNoise from 'simplex-noise';
-import { Quaternion, Vector3 } from 'three';
+import { Vector3 } from 'three';
+import { Box, RangeInput, Text, TextInput } from 'grommet';
 
 interface TerrainCubeProps extends MeshProps {
-  color?: number
+  position: Vector3
 }
 
 const TerrainCube = (props: TerrainCubeProps) => {
-  const { color, ...others } = props
+  const { position } = props
 
-  const ref = useRef<THREE.Mesh>(null!)
   const [hovered, hover] = useState(false)
-  const [clicked, click] = useState(false)
+
+  const getColor = (height: number): string => {
+    if (height === 0) {
+      return 'lightslategrey'
+    }
+    if (height <= 2) {
+      return 'saddlebrown'
+    }
+    return 'seagreen'
+  }
 
   return (
     <mesh
-      {...others}
-      ref={ref}
-      onClick={(event) => click(!clicked)}
+      position={position}
       onPointerOver={(event) => hover(true)}
       onPointerOut={(event) => hover(false)}>
       <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={'hotpink'} opacity={hovered ? 0.5 : 1} />
+      <meshStandardMaterial color={hovered ? 'salmon' : getColor(position.z)} />
     </mesh>
   )
 }
 
-const generateTerrainData = (seed: string, verticalScale: number, xLength: number, yLength: number): Array<Vector3> => {
+const generateTerrainData = (seed: string, verticalScale: number, roughness: number, sideLength: number): Array<Vector3> => {
   const simplex = new SimplexNoise(seed)
 
   const gridData: Array<Vector3> = []
-  for (let x = 0; x < xLength; x++) {
-    for (let y = 0; y < yLength; y++) {
-      const noiseValue = simplex.noise2D(x, y)
-      const zMax = Math.max(Math.floor(noiseValue * verticalScale), 0)
+  for (let x = 0; x < sideLength; x++) {
+    for (let y = 0; y < sideLength; y++) {
+      const noiseValue = (simplex.noise2D(x * roughness, y * roughness) + 1) / 2
+      const zMax = Math.floor(noiseValue * verticalScale)
       for (let z = 0; z <= zMax; z++) {
-        gridData.push(new Vector3(x - (xLength / 2), y - (yLength / 2), z))
+        gridData.push(new Vector3(x - (sideLength / 2), y - (sideLength / 2), z))
       }
     }
   }
@@ -46,38 +52,74 @@ const generateTerrainData = (seed: string, verticalScale: number, xLength: numbe
 }
 
 const VoxelTerrainPage = () => {
-  const seed = 'seed'
-  const verticalScale = 5
-  const xLength = 25
-  const yLength = 25
-  const cameraRadius = 15
+  const [seed, setSeed] = React.useState('seed');
+  const handleSeedChange: ChangeEventHandler<HTMLInputElement> = (event) => setSeed(event.target.value);
+
+  const [verticalScale, setVerticalScale] = React.useState(5);
+  const handleVerticalScaleChange: ChangeEventHandler<HTMLInputElement> = (event) => setVerticalScale(parseInt(event.target.value));
+
+  const [sideLength, setSideLength] = React.useState(30);
+  const handleSideLengthChange: ChangeEventHandler<HTMLInputElement> = (event) => setSideLength(parseInt(event.target.value));
+
+  const [roughness, setRoughness] = React.useState(0.1);
+  const handleRoughnessChange: ChangeEventHandler<HTMLInputElement> = (event) => setRoughness(parseFloat(event.target.value));
 
   const terrainData = generateTerrainData(
     seed,
     verticalScale,
-    xLength,
-    yLength
+    roughness,
+    sideLength
   )
 
-  const Camera = () => {
-    useFrame(({ clock, camera }) => {
-      camera.lookAt(new Vector3(0, 0, 15))
-      camera.position.x = Math.cos(clock.getElapsedTime()) * cameraRadius
-      camera.position.y = Math.sin(clock.getElapsedTime()) * cameraRadius
-      // camera.position.applyQuaternion(new Quaternion().setFromAxisAngle(new Vector3(0, 0, 1), clock.getElapsedTime() / 10000))
-    })
-    return null
-  }
-
   return (
-    <Canvas camera={{position: [0, cameraRadius, 15]}}>
-      <Camera />
-      <ambientLight />
-      <pointLight position={[10, 10, 100]} />
-      {
-        terrainData.map((position) => <TerrainCube key={`${position.x}-${position.y}-${position.z}`} position={position} />)
-      }
-    </Canvas>
+    <React.Fragment>
+      <Canvas camera={{ position: [0, -22, 13] }}>
+        <ambientLight />
+        <pointLight intensity={0.7} position={[10, 10, 10]} />
+        {
+          terrainData.map((position) => <TerrainCube key={`${position.x}-${position.y}-${position.z}`} position={position} />)
+        }
+      </Canvas>
+      <Box className='control-box' width={'medium'}>
+        <Box direction='row'>
+          <Text>Roughness</Text>
+          <RangeInput
+            min={0}
+            max={0.3}
+            step={0.01}
+            value={roughness}
+            onChange={handleRoughnessChange}
+          />
+        </Box>
+        <Box direction='row'>
+          <Text>Size</Text>
+          <RangeInput
+            min={1}
+            max={40}
+            step={1}
+            value={sideLength}
+            onChange={handleSideLengthChange}
+          />
+        </Box>
+        <Box direction='row'>
+          <Text>Height</Text>
+          <RangeInput
+            min={1}
+            max={10}
+            step={1}
+            value={verticalScale}
+            onChange={handleVerticalScaleChange}
+          />
+        </Box>
+        <Box direction='row'>
+          <Text>Seed</Text>
+          <TextInput
+            value={seed}
+            onChange={handleSeedChange}
+          />
+        </Box>
+      </Box>
+    </React.Fragment >
   )
 };
 
